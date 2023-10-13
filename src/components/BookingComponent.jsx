0b8d04f85b6c7cf8af31b1134937a5bbc81d6999
Saurabh Mishra <hey.saurabh.m@gmail.com>
2023-10-13 13:29:06 +0530

    Added components & navigation, implemented routing

---
+++ src/components/BookingComponent.jsx
@@ -0,0 +1,5 @@
+export default function Booking(){
+    return (
+        <h1>Booking Component</h1>
+    );
+}
---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,41 @@
+
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, CardLink } from "reactstrap";
+
+
+export default function Home(){
+    return (
+        <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                    >
+                    <CardBody>
+                        <CardTitle tag="h5">
+                        Card title
+                        </CardTitle>
+                        <CardSubtitle
+                        className="mb-2 text-muted"
+                        tag="h6"
+                        >
+                        Card subtitle
+                        </CardSubtitle>
+                    </CardBody>
+                    <img
+                        alt="Card cap"
+                        src="https://picsum.photos/318/180"
+                        width="100%"
+                    />
+                    <CardBody>
+                        <CardText>
+                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                        </CardText>
+                        <CardLink href="#">
+                        Card Link
+                        </CardLink>
+                        <CardLink href="#">
+                        Another Link
+                        </CardLink>
+                    </CardBody>
+                    </Card>
+    );
+}
